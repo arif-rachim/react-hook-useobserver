86b3ffe2b16ea5f06dec47635a20e109f2469fff
Marsa Emreef <marsa.emreef@gmail.com>
2022-02-26 18:32:50 +0400

    Feat : Update the package.json

---
+++ index.js
@@ -0,0 +1,13 @@
+import { ObserverValue } from "./lib/ObserverValue";
+import { useObserver } from "./lib/useObserver";
+import { useObserverListener } from "./lib/useObserverListener";
+import { useObserverMapper } from "./lib/useObserverMapper";
+import { useObserverValue } from "./lib/useObserverValue";
+export default {
+    useObserverListener,
+    useObserver,
+    ObserverValue,
+    useObserverValue,
+    useObserverMapper
+};
+//# sourceMappingURL=index.js.map
